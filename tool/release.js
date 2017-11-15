77460245d820419465bcdc5a0437aa8bcae6c1ba
任青松(｡◕∀◕｡) <renqingsong@ezbuy.com>
2017-11-15 10:59:12 +0800

    Improve a big work flow!

--- tool/release.js
+++ tool/release.js
@@ -1,1 +1,19 @@
-console.log("HELLO WORDL !")
+// "release": "npm run build && git add . && git commit -m \"/****** release ******/\" && git push ",
+
+const shell = require("shelljs");
+
+shell.echo("Hello Shell");
+
+shell.exec("npm run build");
+
+const dir = shell.cd("../toddmark.github.io");
+if (dir.code !== 0) {
+  shell.echo("目录不存在");
+  shell.exit();
+}
+
+shell.exec("pwd");
+
+shell.cp("-r", "../toddmark.github.io.source/build/*", "../toddmark.github.io");
+
+shell.exec(" git add . && git commit -m \"/****** Auto release from release.js ******/\" && git push ");
