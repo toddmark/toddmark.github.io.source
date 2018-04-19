937573866557c8e81b894a4ea63134884ecf591e
toddmark <toddmark0315@gmail.com>
2018-04-19 23:00:49 +0800

    a

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 import "./index.less";
 
-import Jumbotron from "./utility/Jumbotron"
+import Jumbotron from "./utility/Jumbotron";
 
-let a= 123;
+const a = 123;
+
 export default class Index extends Component<any, any> {
   constructor(props: any) {
     super(props);
-    this.state = {
-    };
-  }
-  componentDidMount() {
+    this.state = {};
   }
 
   render() {
     return (
       <div>
-          <div className="container flex-parent">
+        <div className="container flex-parent">
           {/* 123 */}
           <Jumbotron content={{ title: "Welcome, Visitors!" }} />
         </div>
