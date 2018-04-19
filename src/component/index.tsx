d3d62aaa3e30b8175def3282ebe396665da7ed0a
toddmark <toddmark0315@gmail.com>
2018-04-19 22:50:15 +0800

    a

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -3,6 +3,7 @@ import "./index.less";
 
 import Jumbotron from "./utility/Jumbotron"
 
+let a= 123;
 export default class Index extends Component<any, any> {
   constructor(props: any) {
     super(props);
