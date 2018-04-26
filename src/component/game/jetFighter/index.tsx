5bac4a214d175d4efe862ffa4d5dbe7b609669d2
toddmark <toddmark0315@gmail.com>
2018-04-27 00:20:15 +0800

    update jet fighter

--- src/component/game/jetFighter/index.tsx
+++ src/component/game/jetFighter/index.tsx
@@ -13,12 +13,23 @@ class Stage extends React.Component<{}, IStageState> {
       bbc: "123"
     };
   }
+
+  componentDidMount() {
+    const canvas: any = document.getElementById("stage");
+    const ctx = canvas.getContext("2d");
+    const img = new Image(); // Create new img element
+    img.src = require("./img/jet_fighter.png"); // Set source path
+    img.onload = () => {
+      ctx.drawImage(img, 100, 100, 50, 50);
+    };
+  }
   render() {
     return (
       <div>
         <Nav />
         <div style={{ margin: "0 auto", width: 600 }}>
           <canvas
+            id="stage"
             style={{ border: "1px solid #ccc" }}
             width={600}
             height={600}
