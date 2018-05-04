752fbdf29a91e14c80bc617126f9cbe620007bf6
toddmark <toddmark0315@gmail.com>
2018-05-05 00:01:53 +0800

    update eslint ignore

--- tool/release.js
+++ tool/release.js
@@ -3,12 +3,19 @@
 const shell = require("shelljs");
 const moment = require("moment");
 
-const markdown = `# The Last Release Time: ${moment().format("YYYY-MM-DD HH:mm:ss")} ![IMG](https://picsum.photos/888/300)`
+const markdown = `# The Last Release Time: ${moment().format(
+  "YYYY-MM-DD HH:mm:ss"
+)} ![IMG](https://picsum.photos/888/300)`;
 
 shell.echo("Hello Shell");
 
-if (shell.exec(" test -d '../toddmark.github.io' && echo 'Yes' || echo 'No' ") === "No") {
-  shell.echo("dictionary is empty, please clone this url: https://github.com/toddmark/toddmark.github.io");
+if (
+  shell.exec(" test -d '../toddmark.github.io' && echo 'Yes' || echo 'No' ") ===
+  "No"
+) {
+  shell.echo(
+    "dictionary is empty, please clone this url: https://github.com/toddmark/toddmark.github.io"
+  );
   shell.exit();
 }
 
@@ -20,10 +27,14 @@ shell.exec("find * -delete");
 
 shell.cp("-r", "../toddmark.github.io.source/build/*", "../toddmark.github.io");
 
-if (process.platform.indexOf("win") > -1 )  {
+if (process.platform.indexOf("win") > -1) {
   shell.exec(`echo ${markdown} >> README.md`);
 } else {
   shell.exec(`echo "${markdown}" >> README.md`);
 }
 
-shell.exec(`git add . && git commit -m "(｡◕∀◕｡) 发布日期: ${moment().format("YYYY年MM月DD日 HH:mm:ss")}" && git push `);
+shell.exec(
+  `git add . && git commit -m "(｡◕∀◕｡) 发布日期: ${moment().format(
+    "YYYY年MM月DD日 HH:mm:ss"
+  )}" && git push `
+);
