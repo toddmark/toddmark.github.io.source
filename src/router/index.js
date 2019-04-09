98960d4fd83b7e9feaa09f4645a43b2f5073bc80
toddmark <toddmark0315@gmail.com>
2019-04-10 00:17:58 +0800

    Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: object.

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ import {
   Route,
   Switch,
   // BrowserRouter as Router,
-  HashRouter as Router,
+  HashRouter as Router
 } from "react-router-dom";
 
 import LazyLoader from "./lazyloader";
@@ -41,8 +41,6 @@ import About from "../component/about.jsx";
 //   );
 // }
 
-
-
 // sandbox
 import Sandbox from "../component/sandbox/index.jsx";
 import BinaryTree from "../component/sandbox/binaryTree";
@@ -52,25 +50,23 @@ import Index from "../component/index.tsx";
 import game from "../component/game/game.jsx";
 import JetFighter from "../component/game/jetFighter/index";
 
-class Root extends Component {
-  render() {
-    return (
-      <div>
-        <Router basename="/">
-          <Switch>
-            <Route exact path="/" component={Index} />
-            <Route path="/hello" component={<div>123</div>} />
-            <Route path="/game" component={game} />
-            <Route path="/jetfighter" component={JetFighter} />
-            <Route path="/about" component={About} />
-            <Route exact path="/sandbox" component={Sandbox} />
-            <Route path="/sandbox/binaryTree" component={BinaryTree} />
-            <Route path="/sandbox/randomSelect" component={RandomSelect} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+function Root() {
+  return (
+    <div>
+      <Router basename="/">
+        {/* <Switch>
+          <Route exact path="/" component={Index} />
+          <Route path="/hello" component={Hello} />
+          <Route path="/game" component={game} />
+          <Route path="/jetfighter" component={JetFighter} />
+          <Route path="/about" component={About} />
+          <Route exact path="/sandbox" component={Sandbox} />
+          <Route path="/sandbox/binaryTree" component={BinaryTree} />
+          <Route path="/sandbox/randomSelect" component={RandomSelect} />
+        </Switch> */}
+      </Router>
+    </div>
+  );
 }
 
 export default Root;
