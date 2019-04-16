fedccf8b708f946067ef6a5cd32a8b60b9083ebb
toddmark <toddmark0315@gmail.com>
2019-04-16 18:05:58 +0800

    update word config

--- src/component/utility/WordCard.jsx
+++ src/component/utility/WordCard.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 class WordCard extends React.Component {
+  componentDidMount() {}
+
+  getDetails() {
+    fetch("https://api.shanbay.com/bdc/search/?word=dynamic", {
+      mode: "no-cors"
+    });
+    console.log(this.props.word.text.toLowerCase());
+  }
+
   render() {
     const item = this.props.word;
     return (
-      <span style={{ color: "#eee" }}>{item.text.toLowerCase()}&emsp;</span>
+      <span style={{ color: "#eee" }} onClick={this.getDetails.bind(this)}>
+        {item.text.toLowerCase()}&emsp;
+      </span>
     );
   }
 }
